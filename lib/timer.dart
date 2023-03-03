fb558e1a4f5867d943bf00a48b97f853598e8b12
SayMyName302 <awaismalik066@gmail.com>
2023-03-03 17:22:48 +0500

    final commit

--- lib/timer.dart
+++ lib/timer.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
 
 class TimerApp extends StatefulWidget {
+  
   final Database database;
 
   const TimerApp({Key? key, required this.database}) : super(key: key);
@@ -13,6 +14,7 @@ class TimerApp extends StatefulWidget {
 }
 
 class _TimerAppState extends State<TimerApp> with WidgetsBindingObserver {
+List<Map<String, dynamic>> yearData = [];
   late Timer _timer;
   int _secondsElapsed = 0;
   late DateTime _currentDate;
@@ -27,7 +29,11 @@ class _TimerAppState extends State<TimerApp> with WidgetsBindingObserver {
     _getSecondsElapsed();
     _startTimer();
     displayAllData();
+_loadYearData();
   }
+
+
+
 
   @override
   void dispose() {
@@ -97,10 +103,49 @@ final db = await widget.database;
 final List<Map<String, dynamic>> maps = await db.rawQuery(
   'SELECT SUM(seconds_elapsed) AS total_seconds, date FROM timer WHERE date BETWEEN ? AND ? GROUP BY strftime("%W", date) ORDER BY date DESC',
   [firstDayOfWeek.subtract(Duration(days: 7)).toString().substring(0, 10), lastDayOfWeek.toString().substring(0, 10)],
+  
+  
 );
+
+
+  
+  
 return maps;
 
 }
+Future<List<Map<String, dynamic>>> _getYearData() async {
+  final dateToday = DateTime.now();
+  final currentYear = dateToday.year;
+  final previousYear = currentYear - 1;
+
+  final db = await widget.database;
+  final List<Map<String, dynamic>> maps = await db.rawQuery(
+    'SELECT SUM(seconds_elapsed) AS total_seconds, strftime("%Y", date) AS year FROM timer WHERE strftime("%Y", date) IN (?, ?) GROUP BY year ORDER BY year DESC',
+    [currentYear.toString(), previousYear.toString()],
+  );
+
+  return maps;
+}
+
+
+
+
+
+Future<List<Map<String, dynamic>>> _getMonthData() async {
+  final dateToday = DateTime.now();
+  final currentMonth = dateToday.month;
+  final currentYear = dateToday.year;
+
+  final db = await widget.database;
+  final List<Map<String, dynamic>> maps = await db.rawQuery(
+    'SELECT SUM(seconds_elapsed) AS total_seconds, strftime("%Y-%m", date) AS month FROM timer WHERE strftime("%m", date) >= ? AND strftime("%Y", date) = ? GROUP BY month ORDER BY month DESC',
+    [(currentMonth - 3).toString().padLeft(2, '0'), currentYear.toString()]
+  );
+  return maps;
+}
+
+
+
 
 
   Future<void> displayAllData() async {
@@ -128,6 +173,12 @@ return maps;
 
     return '$hourString$minuteString$secondString';
   }
+   Future<void> _loadYearData() async {
+    final data = await _getYearData();
+    setState(() {
+      yearData = data;
+    });
+  }
 
 @override
 Widget build(BuildContext context) {
@@ -135,34 +186,129 @@ Widget build(BuildContext context) {
     appBar: AppBar(
       title: const Text('Timer App'),
     ),
-    body: FutureBuilder<List<Map<String, dynamic>>>(
-      future: _getWeekData(),
-      builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-        if (snapshot.hasData) {
-          final data = snapshot.data!;
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (BuildContext context, int index) {
-              final date = DateTime.parse(data[index]['date']);
-              final totalSeconds = data[index]['total_seconds'] as int;
-              final duration = Duration(seconds: totalSeconds);
-              final weekText = index == 0 ? 'Current Week' : 'Previous Week';
-              return ListTile(
-                title: Text('$weekText (${DateFormat.yMd().format(date.subtract(Duration(days: date.weekday - 1)))} - ${DateFormat.yMd().format(date.add(Duration(days: 7 - date.weekday)))})'),
-                subtitle: Text('Total time: ${duration.inHours} hours, ${(duration.inMinutes % 60).toString().padLeft(2, '0')} minutes, ${(duration.inSeconds % 60).toString().padLeft(2, '0')} seconds'),
-              );
+    body: SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            'This week and previous week',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(height: 16),
+          FutureBuilder<List<Map<String, dynamic>>>(
+            future: _getWeekData(),
+            builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+              if (snapshot.hasData) {
+                final data = snapshot.data!;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final date = DateTime.parse(data[index]['date']);
+                    final totalSeconds = data[index]['total_seconds'] as int;
+                    final duration = Duration(seconds: totalSeconds);
+                    final weekText = index == 0 ? 'Current Week' : 'Previous Week';
+                    return ListTile(
+                      title: Text('$weekText (${DateFormat.yMd().format(date.subtract(Duration(days: date.weekday - 1)))} - ${DateFormat.yMd().format(date.add(Duration(days: 7 - date.weekday)))})'),
+                      subtitle: Text('Total time: ${duration.inHours} hours, ${(duration.inMinutes % 60).toString().padLeft(2, '0')} minutes, ${(duration.inSeconds % 60).toString().padLeft(2, '0')} seconds'),
+          
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
             },
-          );
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-        return const CircularProgressIndicator();
-      },
-    ),
-  );
+          ),
+
+
+
+          const SizedBox(height: 16),
+          Text(
+            'This month and previous 3 months',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(height: 16),
+          FutureBuilder<List<Map<String, dynamic>>>(
+            future: _getMonthData(),
+            builder: (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+              if (snapshot.hasData) {
+                final data = snapshot.data!;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final month = DateTime.parse('${data[index]['month']}-01');
+                    final totalSeconds = data[index]['total_seconds'] as int;
+                    final duration = Duration(seconds: totalSeconds);
+                    final monthText = index == 0 ? 'Current Month' : 'Previous Month';
+                    return ListTile(
+                      title: Text('$monthText (${DateFormat.yMMM().format(month)})'),
+                      subtitle: Text('Total time: ${duration.inHours} hours, ${(duration.inMinutes % 60).toString().padLeft(2, '0')} minutes, ${(duration.inSeconds % 60).toString().padLeft(2, '0')} seconds'),
+           
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+          
+           const SizedBox(height: 16),
+ 
+     FutureBuilder<List<Map<String, dynamic>>>(
+    future: _getYearData(),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        final yearData = snapshot.data;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (int index = 0; index < yearData!.length; index++)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: _buildYearTile(yearData[index]),
+              ),
+          ],
+        );
+      } else if (snapshot.hasError) {
+        return Text('Error: ${snapshot.error}');
+      } else {
+        return Center(child: CircularProgressIndicator());
+      }
+    },
+        
+  )
+        ]
+      )
+    )
+    );
 }
 
- @override
+ Widget _buildYearTile(Map<String, dynamic> yearData) {
+  final year = yearData['year'];
+  final totalSeconds = yearData['total_seconds'];
+  final duration = Duration(seconds: totalSeconds);
+  final hours = duration.inHours;
+  final minutes = duration.inMinutes.remainder(60);
+  final seconds = duration.inSeconds.remainder(60);
+
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('$year usage:', style: TextStyle(fontWeight: FontWeight.bold)),
+      SizedBox(height: 4.0),
+      Text('$hours hours, $minutes minutes, $seconds seconds'),
+    ],
+  );
+}
+@override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.paused ||
         state == AppLifecycleState.inactive ||
@@ -182,3 +328,9 @@ Widget build(BuildContext context) {
     }
   }
 }
+       
+
+
+
+
+ 
